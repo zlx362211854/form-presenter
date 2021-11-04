60f91c147f0351de3648808e527de494997aa502
zlx <zlx2018@aliyun.com>
2021-11-04 09:59:52 +0800

    feat: export components

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
 import FromPresenter from './FormPresenter'
+import Avatar from './formComponents/Avatar'
 import ImageService from './formComponents/imageService'
+import PicturesWall from './formComponents/PicturesWall'
+import Uploader from './formComponents/upload'
 import FormView from './FormView'
 export default FromPresenter
 export {
+  Avatar,
+  PicturesWall,
+  Uploader,
   ImageService,
   FormView
 }
