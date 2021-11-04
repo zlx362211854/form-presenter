09c12ea220765d5afb004ac8227bd5e93316e342
zlx <zlx2018@aliyun.com>
2021-11-04 08:59:18 +0800

    feat: formView

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import FromPresenter from './FormPresenter'
 import ImageService from './formComponents/imageService'
-
+import FormView from './FormView'
 export default FromPresenter
-export {ImageService}
+export {
+  ImageService,
+  FormView
+}
